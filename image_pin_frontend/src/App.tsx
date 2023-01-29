ff0caf65d806887ceaea72a18418a2e8a48356bf
tmichell13 <tmichell13@yahoo.com>
2023-01-28 21:50:48 -0500

    fixed sidebar issue, there is now an issue with the spinner not rendering

--- image_pin_frontend/src/App.tsx
+++ image_pin_frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path='login' element={<Login />} />
-      <Route path='/' element={<Home />} />
+      <Route path='/*' element={<Home />} />
     </Routes>
   );
 }
--- image_pin_frontend/src/components/Feed.tsx
+++ image_pin_frontend/src/components/Feed.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { client } from '../client'
+import MasonryLayout from './MasonryLayout'
+import Spinner from './Spinner'
 
 const Feed = () => {
+
+  const [loading, setLoading] = useState(false)
+
+  if(loading)
+    return <Spinner message='Adding to your Feed'/>
+
   return (
     <div>
       Feed
